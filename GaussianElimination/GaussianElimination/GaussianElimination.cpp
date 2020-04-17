57a9dba789a61e22b4a29c3f5957b282a48095d8
Mateusz Kulpa <mateusz.kulpa@21infinity.com>
2020-04-17 19:51:41 +0200

    parse and write csv methods

--- GaussianElimination/GaussianElimination/GaussianElimination.cpp
+++ GaussianElimination/GaussianElimination/GaussianElimination.cpp
@@ -1,20 +1,49 @@
-// GaussianElimination.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include <omp.h>
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <list>
+#include "csv.hpp"
+
+using namespace std;
+using namespace csv;
+
+auto const DELIMITER = ';';
+
+template <typename T>
+vector<vector<T>> parse_csv_to_matrix(string path, char delimiter) {
+	CSVFormat format;
+	format.delimiter(delimiter).variable_columns(true);
+	CSVReader reader(path, format);
+	vector<vector<T>> matrix = {};
+
+	for (CSVRow& row : reader) {
+		vector<T> cells = {};
+		for (CSVField& field : row) {
+			cells.push_back(field.get<T>());
+		}
+		matrix.push_back(cells);
+	}
+
+	return matrix;
+}
+
+template <typename T>
+void save_matrix_to_csv(string path, vector<vector<T>> matrix) {
+	ofstream ofs(path);
+	auto writer = make_csv_writer(ofs);
+
+	for (vector<T> const& row : matrix) {
+		vector<string> string_vector = {};
+		transform(begin(row), end(row), back_inserter(string_vector), [](T d) { return to_string(d); });
+		writer << string_vector;
+	}
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	auto matrix = parse_csv_to_matrix<double>("C.csv", DELIMITER);
+	save_matrix_to_csv<double>("test.csv", matrix);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
